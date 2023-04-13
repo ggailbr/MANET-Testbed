9c13e177760317437f1825257e207c67a147dfe2
ggailbr <ggailbr@clemson.edu>
2023-04-13 14:29:36 -0400

    Added Ignore and updated the code

--- rtnetlink_test.c
+++ rtnetlink_test.c
@@ -2,11 +2,13 @@
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <asm/types.h>
+#include <unistd.h>
+#include <net/if.h> /* For index to dev name and back*/
+#include <errno.h>
+#include <sys/uio.h>
 
 int main(int argc, char* argv[]){
 
-
-    int nlskt = socket(AF_NETLINK,SOCK_RAW,NETLINK_ROUTE);
     /*------GENERAL PACKET CREATION--------------------
     struct{
         nlmsgheader
@@ -24,11 +26,43 @@ int main(int argc, char* argv[]){
    struct {
         struct nlmsghdr nlh;
         struct ifaddrmsg addr;
-        char attr[1024];
    }packet;
     // Going to attempt to request IP Address of Wlan0
     // Will require both receiving and requesting
-    // Should probably check the necessity of the msg structure
+    struct sockaddr_nl nladdr = {
+        .nl_family = AF_INET,
+        .nl_pad = 0,
+        .nl_pid = getpid(),
+        .nl_groups = 0};
+    struct iovec iov;
+    struct msghdr msg= {
+        .msg_name = &nladdr,
+        .msg_namelen = sizeof(nladdr),
+        .msg_iov = &iov,
+        .msg_iovlen = 1,
+        .msg__control = NULL,
+        .msg_controllen = 0,
+        .msg_flags = 0};
+    int fd, success, sequence_number = 1;
+
+    fd = socket(AF_NETLINK,SOCK_RAW,NETLINK_ROUTE);
+    if((success =  bind(fd, (struct sockaddr *) &nladdr, sizeof(nladdr))) < 0){
+        printf("Error Binding to Socket\n");
+        return errno;
+    }
+    nladdr.nl_pid = 0;
+    
+    packet.nlh.nlmsg_type = RTM_GETADDR;
+    packet.nlh.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+    packet.nlh.nlmsg_len = NLMSG_LENGTH(sizeof(struct ifaddrmsg));
+    packet.nlh.nlmsg_pid = 0;
+    packet.nlh.nlmsg_seq = sequence_number++;
+    iov.iov_base = (void *)&packet;
+    iov.iov_len = packet.nlh.nlmsg_len;
+    sendmsg(fd,&msg,0);
+    printf("Sent Message");
+
+
 
     return 0;
 }
