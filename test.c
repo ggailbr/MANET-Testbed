9f645f0a75f0d2d4bc0b6ca29d6ba93bd77988c0
Gage Gailbreath <ggailbr@clemson.edu>
2023-11-05 17:41:59 -0500

    fix header offset

--- test.c
+++ test.c
@@ -1,5 +1,13 @@
 // sudo LD_LIBRARY_PATH=/home/pi/Documents/MANET-Testbed:$LD_LIBRARY_PATH ./test.out
 #include "manet_testbed.h"
+
+char *ntop(int domain, void *buf) // convert ip to string
+{
+	static char ip[INET6_ADDRSTRLEN];
+	inet_ntop(domain, buf, ip, INET6_ADDRSTRLEN);
+	return ip;
+}
+
 int main(void)
 {
     char *name = "wlan0";
@@ -9,6 +17,10 @@ int main(void)
 	if((int)a == -1)
 		printf("error");
 	else		
-		printf("my local is: %x\n", a);
+		//printf("my local is: %x\n", a);
+
+	printf("a: %s", ntop(AF_INET, "192.168.1.8"));
+
 	return 0;
+
 }
